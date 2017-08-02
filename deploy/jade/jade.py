8d9dbff6f83012dc8d12b5fcf51a4072698609eb
James Hetherington <jamespjh@googlemail.com>
2017-08-02 15:40:49 +0100

    Fixing bugs in Jade deploy

--- deploy/jade/jade.py
+++ deploy/jade/jade.py
@@ -10,6 +10,7 @@ env.run_at=env.home+"/Scratch/Scaffold/output"
 env.deploy_to=env.home+"/devel/mpi-scaffold"
 env.clone_url="https://github.com/alan-turing-institute/Turing-Fabric-Scaffold.git"
 env.hosts=['jade.hartree.stfc.ac.uk']
+env.modules="module load openmpi"
 
 env.template_file_path=os.path.join(os.path.dirname(__file__),'jade.sh.mko')
 env.script_local_path=os.path.join(os.path.dirname(__file__),'jade.sh')
--- deploy/simple_mpi/simple_mpi.py
+++ deploy/simple_mpi/simple_mpi.py
@@ -6,34 +6,37 @@ def cold(branch='master'):
     run('mkdir -p '+env.deploy_to)
     run('mkdir -p '+env.run_at)
     with cd(env.deploy_to):
-        with path("/home/hut23/install/bin"):
+        with path(env.deploy_to):
             run('git clone '+env.clone_url)
             run('mkdir Turing-Fabric-Scaffold/build')
             with cd('Turing-Fabric-Scaffold/build'):
                 run('git checkout '+branch)
-                run('cmake ../simple_mpi')
-                run('make')
-                run('test/catch')
+                with prefix(env.modules):
+                    run('cmake ../simple_mpi')
+                    run('make')
+                    run('test/catch')
 
 @task
 def warm(branch='master'):
   with cd(env.deploy_to+'/Turing-Fabric-Scaffold/build'):
-      with path("/home/hut23/install/bin"):
+      with path(env.deploy_to):
         run('git checkout '+branch)
         run('git pull')
-        run('cmake ../simple_mpi')
-        run('make')
-        run('test/catch')
+        with prefix(env.modules):
+            run('cmake ../simple_mpi')
+            run('make')
+            run('test/catch')
 
 @task
 def patch():
   with cd(env.deploy_to+'/Turing-Fabric-Scaffold'):
     local('git diff > patch.diff')
     put('patch.diff','patch.diff')
-    with path("/home/hut23/install/bin"):
+    with path(env.deploy_to):
         run('git checkout .')
         run('git apply patch.diff')
         with cd('build'):
-            run('cmake ../simple_mpi')
-            run('make')
-            run('test/catch')
+            with prefix(env.modules):
+                run('cmake ../simple_mpi')
+                run('make')
+                run('test/catch')
